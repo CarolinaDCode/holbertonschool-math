9f87d8471c8b83203ea241c861487a60641a6312
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-07-30 21:57:51 -0700

    Structure Complex1

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * display_complex_number - function that displays the complex numbers
+ * @c: c
+ * Return: Always 0.
+ */
+void display_complex_number(complex c)
+{
+	if (c.im < 0)
+		printf("%.6g - %.6gi\n", c.re, c.im * -1);
+
+	else if (c.im > 0)
+		printf("%.6g + %.6gi\n", c.re, c.im);
+
+	else if (c.im == 0)
+		printf("%.6g\n", c.re);
+
+}
