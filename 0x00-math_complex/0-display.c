267feab944675729e140c68504d724f0b6b08475
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-07-30 22:27:12 -0700

    Structure Complex4

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -10,7 +10,7 @@ void display_complex_number(complex c)
 	if (c.re == 0)
 		printf("%.6gi\n", c.im);
 
-	if (c.im < 0)
+	if (c.im < -1)
 	  printf("%.6g - %.6gi\n", c.re, c.im * -1);
 
 	else if (c.im > 1)
@@ -21,4 +21,7 @@ void display_complex_number(complex c)
 
 	else if (c.im == 1)
 		printf("%.6g + i\n", c.re);
+
+	else if (c.im == -1)
+		printf("%.6g - i\n", c.re);
 }
